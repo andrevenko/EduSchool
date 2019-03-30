7c956f6921abb69035a022e01c898c3d93b26ee2
Andrian1983 <prostudawp@gmail.com>
2019-03-30 21:59:30 +0200

    I have updated a contact form
    I have updated fields in contact form

--- php/send.php
+++ php/send.php
@@ -1,22 +1,22 @@
 <?php
 
 if(isset($_POST['submit'])){
-$to = "andrevenko@gmail.com";; // Здесь нужно написать e-mail, куда будут приходить письма
+$to = "andrevenko@gmail.com";; // Тут треба написати e-mail, куда будут приходити листи
 $from = $_POST['email']; // this is the sender's Email address
 $first_name = $_POST['first_name'];
 $phone = $_POST['tel1'];
-$subject = "Форма отправки сообщений с сайта";
+$subject = "Форма відправки повідомлень із сайту";
 $subject2 = "Copy of your form submission";
-$message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+$message = $first_name . " залишив повідомлення:" . "\n\n" . $_POST['message'];
 $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
 $headers = "From:" . $from;
 $headers2 = "From:" . $to;
 
 mail($to,$subject,$message,$headers,$phone);
-// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
-echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
+// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Відключено!
+echo "Повідомлення відправлено. Дякую Вам " . $first_name . ", ми зв'яжемося з Вами.";
+echo "<br /><br /><a href='https://andrevenko.github.io/EduSchool/index.html'>Повернутися на сайт.</a>";
 
 }
 
